552720bfabbf655f4d1bd5f1a4d2fe298c2a33bd
Fabian Cook <hello@fabiancook.dev>
2023-05-24 10:27:17 +1200

    split research

---
+++ src/lib/posts.ts
@@ -0,0 +1,50 @@
+
+import { readOne, readAll } from "./markdoc/read";
+import { blog } from "./markdoc/frontmatter.schema";
+import { SITE_URL } from "../config";
+
+export async function getPostSlugs() {
+    const posts = await readAll({
+        directory: "blog",
+        frontmatterSchema: blog,
+    });
+
+    // we don't want to generate pages for posts that link to external websites
+    const filteredPosts = posts
+        .filter((p) => p.frontmatter.draft !== true)
+        .filter(({ frontmatter }) => !frontmatter.external);
+
+    //console.log(filteredPosts.map(post => post.slug));
+
+    return filteredPosts.map((post) => {
+        return { params: { slug: post.slug } };
+    })
+}
+
+export async function getPost(slug: string) {
+    if (!slug) {
+        throw Error(`slug should be string. Received: ${slug}`);
+    }
+
+    const posts = await readAll({
+        directory: "blog",
+        frontmatterSchema: blog,
+    });
+
+    const post = posts.find(post => post.slug === slug);
+    if (!post) {
+        throw Error(`Could not find post: ${slug}`);
+    }
+    const { content, frontmatter } = post;
+
+    const ogImageAbsoluteUrl =
+         frontmatter.ogImagePath
+            ? new URL(frontmatter.ogImagePath, SITE_URL).toString()
+            : undefined;
+
+    return {
+        content,
+        ogImageAbsoluteUrl,
+        frontmatter
+    }
+}
